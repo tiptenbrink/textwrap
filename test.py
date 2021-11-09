f93d7a29a865723b82b0ddd619c90905be3cf4a0
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2021-11-09 17:15:52 +0100

    _find_width_index and _handle_lon_word change

--- test.py
+++ test.py
@@ -4,5 +4,17 @@ from cwcwidth import wcswidth as wcw
 print(wcw('a'))
 y = "12⁦⁩⁦⁩⁩⁩⁩345678900"
 print(wcw(y))
-wrpr = wrp.TextWrapper(width=10, text_len=wcw)
+wrpr = wrp.TextWrapper(width=6, text_len=wcw)
 print(wrpr.wrap(y))
+
+def text_len(text):
+    return sum(
+        0 if c == 'Q' else 1
+        for c in text
+    )
+
+
+wrappd = wrp.TextWrapper(width=6, text_len=text_len)
+print(wrappd.wrap("012345678"))
+print(wrappd.wrap("01QQQQ2Q345678"))
+print(wrappd.wrap("QQQQQ01234"))
